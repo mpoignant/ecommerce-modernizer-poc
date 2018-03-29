3ac84f9f7f5bfbdc8c1b66f44aa41a42ca4eac48
Mathieu Poignant <mathieu.poignant@gmail.com>
2018-03-29 15:45:46 +0200

    tools cleanup

--- tools/src/main/java/com/octo/ecommodpoc/domain/InvalidHotelException.java
+++
@@ -1,7 +0,0 @@
-package com.octo.ecommodpoc.domain;
-
-public class InvalidHotelException extends RuntimeException {
-    public InvalidHotelException(String message) {
-        super(message);
-    }
-}
